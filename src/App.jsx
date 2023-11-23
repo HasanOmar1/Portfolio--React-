c5c4e7a5a51f43b9d0094f28c04e95d209d95aca
HasanOmar1 <r3domar1@gmail.com>
2023-11-23 17:10:20 +0200

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Navbar from "./Components/Navbar/Navbar"
+import Hero from "./Components/Hero/Hero.jsx"
 
 function App() {
 
   return (
     <>
      <Navbar />
+     <main>
+      <Hero />
+     </main>
     </>
   )
 }
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './hero.css'
+
+export default function Hero() {
+  return (
+    <div className='welcome-page'>
+        <h1 className='welcome-title'>WELCOME TO MY PORTFOLIO</h1>
+    </div>
+  )
+}
