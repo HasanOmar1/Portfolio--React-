b42a754a33e7d3b50f280dc5881f287d356fb405
HasanOmar1 <r3domar1@gmail.com>
2023-11-24 23:57:04 +0200

    added

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
     <main id='home' className='welcome-page'>
         <h1 className='welcome-title'>
           <span id='welcome-word'>WELCOME</span>
-           TO MY 
+           <span id='to-my'>TO MY</span>
            <span id='welcome-word'>PORTFOLIO</span></h1>
         <div className='avatar'></div>
     </main>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,18 +3,30 @@ import './navbar.css'
 
 export default function Navbar() {
   return (
-<nav>
-<div className="nav-bar">
+<nav  className="nav-bar">
+
     <div className="logo">
         <a  href="#home" id='logo'>HASAN</a>
     </div>
-    <div className="info-container">
+
+    <input id='menu-toggle' type="checkbox" />
+    <label class='menu-button-container' for="menu-toggle">
+    <div class='menu-button'></div>
+
+    </label>
+    {/* <div className="info-container">
     <a href="#home">Home</a>
     <a href="#about">About</a>
     <a href="#projects">Projects</a>
     <a href="#contact">Contact</a>       
-    </div>
-</div>
+    </div> */}
+    <ul class="menu">
+      <li><a href="#home">Home</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#projects">Projects</a></li>
+      <li><a href="#contact">Contact</a></li>
+    </ul>
+
 </nav>
   )
 }
