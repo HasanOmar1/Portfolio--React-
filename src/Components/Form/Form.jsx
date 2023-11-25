3a898089d12d75375e184f5aae2adaada4a05d2e
HasanOmar1 <r3domar1@gmail.com>
2023-11-25 16:42:56 +0200

    added

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import './form.css'
 
+
 export default function Form() {
 
-    function handleClick(){
+    function handleClick(e){
+        e.preventDefault()
         alert(`Form Submitted!`)
+        document.getElementById('myForm').reset();
     }
+
 
   return (
     <section id='contact' className='form-section'>
         <h1>Contact Me</h1>
         <div className="form-container">
-        <form>
+        <form id='myForm' onSubmit={handleClick}>
             <div className="name-form size">
             <label>Name:</label>
             <input type="text" required />
@@ -24,7 +28,7 @@ export default function Form() {
             <div className="textarea-form size">
                 <textarea id='textarea'  cols="50" rows="6" required></textarea>
             </div>
-            <input className='submit-btn' type="submit" onClick={handleClick} />
+            <input className='submit-btn' type="submit"  />
         </form>
         </div>
     </section>
