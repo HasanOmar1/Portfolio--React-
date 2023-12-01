e1594c21e83cb516e9ec8440f541125d94056fe8
HasanOmar1 <r3domar1@gmail.com>
2023-12-01 15:50:53 +0200

    updated

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,34 +1,80 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./form.css";
 
 export default function Form() {
-  function handleClick(e) {
+  const [formInfo, setFormInfo] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const nameInput = useRef();
+  const emailInput = useRef();
+  const messageInput = useRef();
+
+  function handleSubmit(e) {
     e.preventDefault();
-    alert(`Form Submitted!`);
-    document.getElementById("myForm").reset();
+    setFormInfo({
+      ...formInfo,
+      name: nameInput.current.value,
+      email: emailInput.current.value,
+      message: messageInput.current.value,
+    });
+    alert(`Thank you ${nameInput.current.value} for contacting me!`);
+    localStorage.setItem("name", nameInput.current.value);
+    localStorage.setItem("email", emailInput.current.value);
+    localStorage.setItem("message", messageInput.current.value);
+    nameInput.current.value = "";
+    emailInput.current.value = "";
+    messageInput.current.value = "";
   }
 
   return (
     <section id="contact" className="form-section">
       <h1>Contact Me</h1>
-      <div className="form-container">
-        <form id="myForm" onSubmit={handleClick}>
-          <div className="name-form size">
-            <label>Name:</label>
-            <input type="text" required />
+      <div className="welcoming-form-container">
+        <div className="form-container">
+          <form id="myForm" onSubmit={handleSubmit}>
+            <div className="name-form size">
+              <label>Name:</label>
+              <input
+                type="text"
+                required
+                ref={nameInput}
+                defaultValue={formInfo.name}
+              />
+            </div>
+            <div className="email-form size">
+              <label>Email:</label>
+              <input
+                type="email"
+                required
+                ref={emailInput}
+                defaultValue={formInfo.email}
+              />
+            </div>
+            <label id="message" className="size">
+              Message:
+            </label>
+            <div className="textarea-form size">
+              <textarea
+                id="textarea"
+                cols="50"
+                rows="6"
+                required
+                ref={messageInput}
+                defaultValue={formInfo.message}
+              ></textarea>
+            </div>
+            <input className="submit-btn" type="submit" />
+          </form>
+          <div className="form-text">
+            {/* <p className="form-welcoming">
+              Thank you {formInfo.name} for contacting me I will respond to you
+              soon!
+            </p> */}
           </div>
-          <div className="email-form size">
-            <label>Email:</label>
-            <input type="email" required />
-          </div>
-          <label id="message" className="size">
-            Message:
-          </label>
-          <div className="textarea-form size">
-            <textarea id="textarea" cols="50" rows="6" required></textarea>
-          </div>
-          <input className="submit-btn" type="submit" />
-        </form>
+        </div>
       </div>
     </section>
   );
