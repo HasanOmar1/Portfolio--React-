1ccb5d67d8e743cd9a6f4fad5e19aea2fc2f53b0
HasanOmar1 <r3domar1@gmail.com>
2023-12-01 15:02:35 +0200

    updated

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,36 +1,35 @@
-import React from 'react'
-import './form.css'
-
+import React from "react";
+import "./form.css";
 
 export default function Form() {
-
-    function handleClick(e){
-        e.preventDefault()
-        alert(`Form Submitted!`)
-        document.getElementById('myForm').reset();
-    }
-
+  function handleClick(e) {
+    e.preventDefault();
+    alert(`Form Submitted!`);
+    document.getElementById("myForm").reset();
+  }
 
   return (
-    <section id='contact' className='form-section'>
-        <h1>Contact Me</h1>
-        <div className="form-container">
-        <form id='myForm' onSubmit={handleClick}>
-            <div className="name-form size">
+    <section id="contact" className="form-section">
+      <h1>Contact Me</h1>
+      <div className="form-container">
+        <form id="myForm" onSubmit={handleClick}>
+          <div className="name-form size">
             <label>Name:</label>
             <input type="text" required />
-            </div>
-            <div className="email-form size">
-                <label>Email:</label>
-                <input type="email" required/>
-            </div>
-                <label id='message' className='size'>Message:</label>
-            <div className="textarea-form size">
-                <textarea id='textarea'  cols="50" rows="6" required></textarea>
-            </div>
-            <input className='submit-btn' type="submit"  />
+          </div>
+          <div className="email-form size">
+            <label>Email:</label>
+            <input type="email" required />
+          </div>
+          <label id="message" className="size">
+            Message:
+          </label>
+          <div className="textarea-form size">
+            <textarea id="textarea" cols="50" rows="6" required></textarea>
+          </div>
+          <input className="submit-btn" type="submit" />
         </form>
-        </div>
+      </div>
     </section>
-  )
+  );
 }
