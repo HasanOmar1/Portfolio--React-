a32dfcb7f41f7a62a748fb96f00a5c85ad57bc45
HasanOmar1 <r3domar1@gmail.com>
2023-12-01 14:25:03 +0200

    updated

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./hero.css";
 import "../../assets/cat.jpg";
 import { FaArrowUp } from "react-icons/fa";
+import About from "../About/About";
+import Footer from "../Footer/Footer";
+import Projects from "../Projects/Projects";
+import Form from "../Form/Form";
 
 export default function Hero() {
+  const [infoPage, setInfoPage] = useState("about-me");
+
+  function handleOnClick(status) {
+    setInfoPage(status);
+  }
+
   return (
     <main id="home" className="welcome-page">
       <h1 className="welcome-title">
@@ -15,6 +25,46 @@ export default function Hero() {
       <a href="#home" id="go-top">
         <FaArrowUp />
       </a>
+
+      <div className="button-info-container">
+        <button
+          className={`about-btn btn-info-styles ${
+            infoPage === "about-me" ? "active-btn" : ""
+          } `}
+          onClick={() => handleOnClick("about-me")}
+        >
+          About Me
+        </button>
+        <button
+          className={`project-btn btn-info-styles ${
+            infoPage === "projects" ? "active-btn" : ""
+          }`}
+          onClick={() => handleOnClick("projects")}
+        >
+          Couple of My Projects
+        </button>
+        <button
+          className={`contact-btn btn-info-styles ${
+            infoPage === "contact-me" ? "active-btn" : ""
+          }`}
+          onClick={() => handleOnClick("contact-me")}
+        >
+          Contact Me
+        </button>
+      </div>
+
+      <div className="render-page">
+        {infoPage === "about-me" ? (
+          <About />
+        ) : infoPage === "projects" ? (
+          <Projects />
+        ) : infoPage === "contact-me" ? (
+          <Form />
+        ) : (
+          <About />
+        )}
+      </div>
+      <Footer />
     </main>
   );
 }
--- src/Components/My-Site/Site.jsx
+++ src/Components/My-Site/Site.jsx
@@ -1,20 +1,12 @@
-import React from 'react'
-import Navbar from '../../Components/Navbar/Navbar.jsx'
-import Hero from "../../Components/Hero/Hero.jsx"
-import About from "../../Components/About/About.jsx"
-import Projects from "../../Components/Projects/Projects.jsx"
-import Form from "../../Components/Form/Form.jsx"
-import Footer from "../../Components/Footer/Footer.jsx"
+import React from "react";
+import Navbar from "../../Components/Navbar/Navbar.jsx";
+import Hero from "../../Components/Hero/Hero.jsx";
 
 export default function Site() {
   return (
     <>
-     <Navbar />
+      <Navbar />
       <Hero />
-      <About />
-      <Projects />
-      <Form />
-      <Footer />
     </>
-  )
+  );
 }
